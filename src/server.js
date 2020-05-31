6554b1f29f15161addd0a2ea50ac83b8c4763497
Gabrielda31 <gabrielda3108@gmail.com>
2020-05-31 23:00:23 +0700

    add mongoose and connect to mongodv

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = require('../config/keys').mongoURI;
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('MongoDB Successfully Connected!');
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 app.get('/', (req, res) => {
-    res.send('Hello World!'); 
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${port}`);
 });
-
