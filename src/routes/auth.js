076747a28940d73de9928e9ff6dff709db48c2e6
Gabrielda31 <gabrielda3108@gmail.com>
2020-06-11 23:13:30 +0700

    Register function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const User = mongoose.model()
+const User = require('../models/user');
 
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -13,21 +13,47 @@ router.get('/', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-  console.log(req.body);
-  // const {
-  //   firstName,
-  //   lastName,
-  //   email,
-  //   password
-  // } = req.body;
-  // if (!firstName || !lastName || !email || !password) {
-  //   return res.status(422).json({
-  //     error: "Please fill all the required field."
-  //   });
-  // }
-  // return res.json({
-  //   message: "Sign up successfully."
-  // });
+  const {
+    firstName,
+    lastName,
+    email,
+    password
+  } = req.body;
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(422).json({
+      error: "Please fill all the required field."
+    });
+  }
+  User.findOne({
+      email: email
+    })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({
+          error: "User already exists with that email."
+        })
+      }
+
+      const user = new User({
+        email,
+        password,
+        firstName,
+        lastName
+      });
+
+      user.save()
+        .then(user => {
+          res.json({
+            message: "Saved successfully."
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 });
 
 module.exports = router;
