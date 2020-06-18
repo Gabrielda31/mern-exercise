fb3c663937a1f74b43f3ed8dd354f859024e3e59
Gabrielda31 <gabrielda3108@gmail.com>
2020-06-18 22:48:52 +0700

    login function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,6 +7,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const keys = require('../../config/keys');
 const validateRegisterInput = require('../validator/register');
+const validateLoginInput = require('../validator/login');
+const { secretOrKey } = require('../../config/keys');
 
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
@@ -28,7 +30,6 @@ router.post('/register', (req, res) => {
   if (!isValid) {
     return res.status(400).json(errors);
   }
-  console.log(req.body);
   User.findOne({
       email: req.body.email
     })
@@ -48,7 +49,7 @@ router.post('/register', (req, res) => {
         // Hash password before saving in database
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
-            // if (err) throw err;
+            if (err) throw err;
             newUser.password = hash;
             newUser.save()
               .then(user => {
@@ -82,6 +83,54 @@ router.post('/register', (req, res) => {
         });
       }
     });
+});
+
+router.post('/login', (req, res) => {
+  //Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+  //Check validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  // Find user by email
+  User.findOne({email}).then(user => {
+    //Check if user exists
+    if(!user) {
+      return res.status(404).json({usernotfound: 'User not found.'});
+    }
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if(isMatch) {
+        //User matched
+        //Create JWT payload
+        const payload = {
+          id: user.id,
+          name: user.firstName + ' ' + user.lastName
+        };
+        //Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token,
+              name: user.firstName + user.lastName,
+              email: user.email
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({passwordincorrect: 'Password incorrect.'});
+      }
+    });
+  });
 });
 
 module.exports = router;
